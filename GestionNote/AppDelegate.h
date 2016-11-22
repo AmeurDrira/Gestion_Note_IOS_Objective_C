cf447df955f845d4bef45dc73f4c94d29b00af1a
ameur <ameur.drira@gmail.com>
2016-11-22 14:28:05 +0100

    Initial Commit

---
+++ GestionNote/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GestionNote
+//
+//  Created by Ameur Drira on 22/11/2016.
+//  Copyright © 2016 IIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GestionNote/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GestionNote
+//
+//  Created by Ameur Drira on 22/11/2016.
+//  Copyright © 2016 IIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
